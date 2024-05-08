3cf2c12da1eadfbe5374839ad952bd727c07e7df
RamprasadRajasegar <ramprasadrajasegar@gmail.com>
2024-05-08 23:17:41 +0530

    Initial commit

---
+++ src/main/java/com/project/PollingWebApp/PollingWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.PollingWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/PollingWebApp/PollingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PollingWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
