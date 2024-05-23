f25a39f5abb5eb8fcc595c102760800fd37bb23e
RamprasadRajasegar <ramprasadrajasegar@gmail.com>
2024-05-24 00:49:06 +0530

    FEAT AuthenticationRequest & Response

---
+++ src/main/java/com/project/PollingWebApp/config/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.project.PollingWebApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+//Planning to use security filter chain
+}
