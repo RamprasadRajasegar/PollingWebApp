42a64eaf19adb09a2a74b6ccd7396b4f491bc2f9
RamprasadRajasegar <ramprasadrajasegar@gmail.com>
2024-06-07 00:33:18 +0530

    FEAT generate,validate,extract_username,expirationdate

--- src/main/java/com/project/PollingWebApp/config/JwtUtil.java
+++ src/main/java/com/project/PollingWebApp/config/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.project.PollingWebApp.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
@@ -7,17 +8,19 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
+    String secret = "secret";
 
     private String createToken(Map<String,Object> claims, String subject){
-        String secret = "secret";
+
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setIssuedAt(new Date(System.currentTimeMillis() + 36000000))
+                .setExpiration(new Date(System.currentTimeMillis() + 36000000))
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
@@ -25,5 +28,31 @@ public class JwtUtil {
     private String generateToken(String subject){
         Map<String, Object> claims=new HashMap<>();
         return createToken(claims,subject);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimResolver){
+        final Claims claims=extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public Date expirationDate(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    public Boolean isTokenExpired(String token){
+        return expirationDate(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, String username){
+        String extractedUsername=extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
     }
 }
--- src/test/java/com/project/PollingWebApp/PollingWebAppApplicationTests.java
+++ src/test/java/com/project/PollingWebApp/PollingWebAppApplicationTests.java
@@ -1,13 +1,21 @@
 package com.project.PollingWebApp;
 
+import com.project.PollingWebApp.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PollingWebAppApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+
+
+
 	@Test
-	void contextLoads() {
+	public String sampleTest(){
+		return "sample";
 	}
 
 }
